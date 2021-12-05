411b652eaf8c1bcfad8574d9d40deb6b25fbd83e
Leehyunjeong <68942045+Hyun2gi@users.noreply.github.com>
2021-12-05 13:49:40 +0900

    Create TitleMenu.cs

---
+++ TitleMenu.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleMenu : MonoBehaviour
+{
+    
+   public void BtnPlay()
+    {
+        GameManager.instance.GameStart();
+        this.gameObject.SetActive(false);
+        
+    }
+
+    public void BtnQuit(){
+        GameManager.instance.GameQuit();
+    }
+
+}
